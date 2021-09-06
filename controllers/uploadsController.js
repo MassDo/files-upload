8eb60c134217acdeedd3a75678c2717c3395ee5f
massdo <dorian_massoulier@hotmail.com>
2021-09-06 17:10:21 +0200

    add checkc for upload image check if file is present heck if file is an image check if image size is less than 1GB

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,7 +1,18 @@
 const path = require('path')
 const {StatusCodes} = require('http-status-codes')
+const customError = require('../errors')
 
 const uploadProductImage = async (req, res) =>{
+    if(!req.files){
+        throw new customError.BadRequestError('Please upload a file')
+    }
+    if(!req.files.image.mimetype.startsWith('image')){
+        throw new customError.BadRequestError('Please upload an image not other file ! :)')
+    }
+    const maxSize = 1024 * 1024
+    if(req.files.image.size > maxSize){
+        throw new customError.BadRequestError('Sorry but the image is too big ... :(')
+    }
     const productImage = req.files.image
     const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`)
     await productImage.mv(imagePath)
